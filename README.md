6b9763ee71b3dd917e8fc1479b576d7894696471
nilsjenn <93837106+nilsjenn@users.noreply.github.com>
2023-04-07 21:17:01 +0200

    README.md added and database paths updated

---
+++ README.md
@@ -0,0 +1,25 @@
+# Python Things Database Connector
+
+The Python Things Database Connector is a project that enables users to interact with their tasks in the Things app for Mac and iPhone through Python. With this connector, users can read and write data, visualize summaries, and manipulate their tasks programmatically.
+
+### Features
+
+Read and write task data in the Things app
+Retrieve summaries of completed and upcoming tasks
+Programmatically create, update, and delete tasks
+
+### Installation
+
+To use the Python Things Database Connectors, follow these steps:
+
+Install the pythings package: pip install pythings.
+Import the package in your Python script: import pythings.
+
+### Usage
+
+Here are some examples of how to use the Python Things Database Connectors:
+
+
+### License
+
+This project is licensed under the MIT License.
--- things-connector.py
+++ things-connector.py
@@ -8,20 +8,21 @@ import datetime as dt
 import plotly.express as px
 import numpy as np
 import dtale
-import dash as dash
 from dash import html
 import csv
-from dash.dependencies import Input, Output, State
+
 import os
 import shutil
-import matplotlib.pyplot as plt
 import seaborn as sns
 import time as time
 import datetime
-from datetime import datetime
+import matplotlib.pyplot as plt
 import plotly.graph_objects as go
+
+import dash as dash
 from dash import dcc
 from flask import request
+from dash.dependencies import Input, Output, State
 from credentials import user
 
 #%% Connect to Database
@@ -44,78 +45,66 @@ data3 = pd.read_sql_query(query3, sql_connect)
 #%% Close connection
 sql_connect.close()
 
-
 #%% Calculating KPIs
 tasks_number = data.groupby('project')['title'].agg('count')
 print(tasks_number)
 
-
 #%% Preparing Data
 
-#
-data_area = data[['uuid', 'area', 'title']]
-data_area = data_area[data_area.area.notnull()]
-data_area = data_area.sort_values('uuid')
-data_area = data_area.rename(columns={'uuid':'area_id', 'title':'area_title'})
-
-data_projects = data[['uuid', 'project', 'title']]
-data_projects = data_projects[data_projects.project.notnull()]
+data_projects = data[['uuid', 'title', 'area']]
+data_projects = data_projects[data_projects.area.notnull()]
 data_projects = data_projects.sort_values('uuid')
-data_projects = data_projects.rename(columns={'uuid':'projects_id', 'title':'project_title'})
+data_projects = data_projects.rename(columns={'uuid':'project_id', 'title':'project_title', 'area':'block_id'})
 
-data_action = data[['uuid', 'actionGroup', 'title']]
-data_action = data_action[data_action.actionGroup.notnull()]
-data_action = data_action.sort_values('uuid')
-data_action = data_action.drop_duplicates("actionGroup")
-data_action = data_action.rename(columns={'uuid':'actions_id', 'title':'action_title'})
+data_area = data[['uuid', 'title', 'project']]
+data_area = data_area[data_area.project.notnull()]
+data_area = data_area.sort_values('uuid')
+data_area = data_area.rename(columns={'uuid': 'area_id', 'title': 'area_title'})
 
+data3 = data3.rename(columns={'uuid': 'block_id', 'title': 'block_title'})
 
-#%% Joining Data
+#%% Joining Data areas
 
-data_joined0 = pd.merge(data_area, data3, how='left', left_on='area', right_on='uuid')
-data_joined0 = data_joined0.rename(columns=({'uuid':'box_id', 'title':'block_title'}))
-data_joined0 = data_joined0[['area_id', 'area_title', 'block_title']]
-data_joined = pd.merge(data, data_joined0, how='left' , left_on='area', right_on='area_id')
+data_joined0 = pd.merge(data, data_area, how='left', left_on='actionGroup', right_on='area_id')
+data_joined0['project_id'] = np.where(data_joined0['project_y'].isnull(), data_joined0['project_x'], data_joined0['project_y'])
+data_joined0 = data_joined0.rename(columns=({'uuid':'task_id', 'title':'task_title', 'actionGroup':'area_id_x'}))
+data_joined0 = data_joined0[['task_id', 'task_title', 'notes', 'area_id', 'area_title', 'project_id', 'userModificationDate', 'creationDate', 'dueDate', 'startDate', 'stopDate', 'status', 'type']]
+
+#%% Joining data projects
+
+data_joined = pd.merge(data_joined0, data_projects, how='left', left_on='project_id', right_on='project_id')
 # data_joined = data_joined[data_joined..notnull()]
-#%%
-data_joined2 = pd.merge(data_joined, data_area, how='left', left_on='project', right_on='area_id')
-#data_joined2 = data_joined2[data_joined2..notnull()]
-
-#data_joined2 = data_joined2[['uuid', 'title', 'notes', 'title_x', 'title_y', 'actionGroup', 'userModificationDate', 'creationDate', 'dueDate', 'startDate', 'stopDate', 'status', 'type']]
-#%%
-data_joined3 = pd.merge(data_joined2, data_action, how='left', left_on='actionGroup', right_on='actionGroup')
-#data_joined3 = data_joined3[data_joined3.uuid.notnull()]
 
 #%%
-data_arranged = data_joined3[['uuid',
-                              'title',
+data_joined2 = pd.merge(data_joined, data3, how='left', left_on='block_id', right_on='block_id')
+
+#%%
+data_arranged = data_joined2[['task_id',
+                              'task_title',
                               'notes',
-                              'action_title',
-                              'area_title_y',
-                              'status',
+                              'area_title',
+                              'area_id',
+                              'project_title',
+                              'project_id',
+                              'block_title',
+                              'block_id',
                               'type',
+                              'status',
                               'userModificationDate',
                               'creationDate',
                               'dueDate',
                               'startDate',
                               'stopDate']]
-data_arranged = data_arranged.rename(columns={'uuid':'task_id',
-                                             'title':'title',
-                                             'notes':'notes',
-                                             'action_title':'action',
-                                             'area_title_y':'area',
-                                             'title_y':'project',
-                                             'status':'status',
-                                             'type':'type'})
 
 
+
+#%% Dtale Data Exploration
+dtale.show(data_joined2)
 
 #%% Converting Types
 convert_dict = {'task_id': str,
-                 'title': str,
+                 'task_title': str,
                  'notes': str,
-                 'action': str,
-                 'area': str,
                  'userModificationDate': float,
                  'creationDate': float,
                  'dueDate': float,
@@ -123,23 +112,17 @@ convert_dict = {'task_id': str,
                  'stopDate': float,
                  'status': int,
                  'type': int}
+
 data_labelled = data_arranged.astype(convert_dict)
 
-#%%
+#%% Converting Timestamps
 data_labelled['creationDate'] = pd.to_datetime(round(data_labelled['creationDate']), format=None, unit='s', errors='coerce')
 data_labelled['startDate'] = pd.to_datetime(round(data_labelled['startDate']), format=None, unit='s', errors='coerce')
 data_labelled['userModificationDate'] = pd.to_datetime(round(data_labelled['userModificationDate']), format=None, unit='s', errors='coerce')
 data_labelled['dueDate'] = pd.to_datetime(round(data_labelled['dueDate']), format=None, unit='s', errors='coerce')
 data_labelled['stopDate'] = pd.to_datetime(round(data_labelled['stopDate']), format=None, unit='s', errors='coerce')
 
-
-print(type(data_labelled['startDate']))
-print(data_labelled['startDate'])
-
-count = data_labelled['title'].value_counts()
-print(count)
-
-#%%
+#%% Mapping Values
 data_labelled['status'] = data_labelled['status'].map({3:'gelöscht', 2:'unkown', 1:'unknown2'})
 data_labelled['type'] = data_labelled['type'].map({2:'area', 1:'block', 0:'task'})
 
@@ -147,35 +130,49 @@ data_labelled['type'] = data_labelled['type'].map({2:'area', 1:'block', 0:'task'
 
 data_labelled['randCol'] = np.random.randint(1, 5, data_labelled.shape[0])
 data_labelled['randCol2'] = np.random.randint(1, 1000, data_labelled.shape[0])
-data_labelled
 
-data_labelled['area'] = data_labelled['area'].fillna(0)
-data_labelled['action'] = data_labelled['action'].fillna(0)
+data_labelled['project_title'] = data_labelled['project_title'].fillna(0)
+data_labelled['area_title'] = data_labelled['area_title'].fillna(0)
+data_labelled['block_title'] = data_labelled['block_title'].fillna(0)
+data_labelled['status'] = data_labelled['status'].fillna(0)
+
+
+#%% Subsetting Data
+subset_tasks = data_labelled[data_labelled['type'] == 'task']
+subset_tasks = subset_tasks[['task_id', 'task_title', 'notes', 'area_title', 'project_title', 'project_id', 'block_title', 'status', 'type', 'userModificationDate', 'creationDate', 'dueDate', 'startDate', 'stopDate']]
+
+subset_area = data_labelled[data_labelled['type'] == 'area']
+subset_area = subset_area[['task_id', 'task_title',  'area_id', 'project_id', 'area_title', 'status', 'type', 'userModificationDate', 'creationDate', 'dueDate', 'startDate', 'stopDate']]
+
+subset_project = data_labelled[data_labelled['type'] == 'block']
+subset_project = subset_project[['task_id', 'task_title', 'notes', 'status', 'type', 'userModificationDate', 'creationDate', 'dueDate', 'startDate', 'stopDate']]
 
 #%% Plotting
-data_labelled= data_labelled[(data_labelled.type == 'task') & (data_labelled.status != 'gelöscht')]
-TaskScatter = px.scatter(data_labelled,
+plotting_data = data_labelled[(data_labelled.status != 'gelöscht')]
+TaskScatter = px.scatter(plotting_data,
                          x="creationDate",
                          y="randCol2",
-                         color='area',
-                         size='randCol2',
-                         symbol='area',
-                         text="title",
-                         opacity=0.5,
-                         hover_data=["area", 'action'],
-                         width=1600,
-                         height=800)
+                         color="block_title",
+                         size="randCol",
+                         hover_name="task_title",
+                         hover_data=["project_title", "area_title", "block_title",
+                                     "status", "type", "userModificationDate", "creationDate",
+                                     "dueDate", "startDate", "stopDate"])
 
+
+TaskScatter.for_each_trace(
+    lambda trace: trace.update(marker_symbol="square") if trace.name == "ARBEIT" else (),
+)
 
 # Use date string to set xaxis range
-TaskScatter.update_layout(xaxis_range=['2022-01-01', '2022-07-31'],
-                          title_text="Manually Set Date Range", uniformtext_minsize=8, uniformtext_mode='hide')
+TaskScatter.update_layout(xaxis_range=['2022-01-01', '2022-07-31'], title_text="Manually Set Date Range", uniformtext_minsize=8, uniformtext_mode='hide')
 
 TaskScatter.update_traces(textposition='top right')
 
 # Rangeslider
 TaskScatter.update_xaxes(
     rangeslider_visible=True,
+    showgrid=False,
     rangeselector=dict(
         buttons=list([
             dict(count=1, label="1m", step="month", stepmode="backward"),
@@ -188,7 +185,7 @@ TaskScatter.update_xaxes(
 )
 
 # Showing the plot
-#TaskScatter.show()
+TaskScatter.show()
 
 #%% Dash App
 # Create the Dash app
@@ -202,23 +199,23 @@ app.layout = html.Div(children=[
                     multi=True),
     dcc.Graph(id='TaskScatter')])
 
-  # Set up callback function
+# Set up callback function
 @app.callback(
     Output(component_id='TaskScatter', component_property='figure'),
     Input(component_id='track-dropdown', component_property='value')
 )
 def update_figure(track):
     filtered_track = data_labelled[data_labelled['area'] == track]
-    TaskScatter = px.scatter(filtered_track,
-                             title=f'Aufgabensteuerung: {track}',
+    TaskScatter = px.scatter(data_labelled,
+                             title=f'Task Scatter {track}',
                              x="creationDate",
-                             y="randCol2",
-                             color='area',
+                             y="randCol",
+                             color='task_title',
                              size='randCol2',
-                             symbol='area',
+                             symbol='',
                              text="title",
                              opacity=0.5,
-                             hover_data=["area", 'action'],
+                             hover_data=["block_title", 'project_title'],
                              width=1600,
                              height=800)
     return TaskScatter
